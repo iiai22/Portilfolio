20a37280e29c7378a2df8c9c77e00489628a300b
iiai22 <114829127+iiai22@users.noreply.github.com>
2023-07-06 18:44:44 -0300

    3 files modified, 1 added, 1 deleted

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ document.addEventListener('DOMContentLoaded', function () {
     navigator.clipboard
       .writeText(emailText)
       .then(function () {
-        console.log('Email copied to clipboard: ' + emailText);
-        alert('Email copied to clipboard: ' + emailText);
+        console.log('E-mail copiado com sucesso: ' + emailText);
+        alert('E-mail copiado com sucesso: ' + emailText);
       })
       .catch(function (error) {
-        console.error('Failed to copy email to clipboard: ', error);
-        alert('Failed to copy email to clipboard. Please try again.');
+        console.error('Falha ao copiar e-mail: ', error);
+        alert('Falha ao copiar e-mail. Por favor tente novamente');
       });
   });
 });
